ea64df5d6c909c62afa6f84c32efd7bcafafb652
miaojingjing <1049089932@qq.com>
2018-11-26 19:11:38 +0800

    tianjia

--- git-test-project/src/com/itheima/git/FirstClsaa.java
+++ git-test-project/src/com/itheima/git/FirstClsaa.java
@@ -4,4 +4,8 @@ public class FirstClsaa {
     public static void main(String[] args) {
         System.out.println("Hello GIT");
     }
+    public void test(){
+        int i =0 ;
+        i ++ ;
+    }
 }
